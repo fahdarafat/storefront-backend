f26f0a8e85b76b00dd583c2cb027fb608c894475
Fahd Arafat <fahdarft@gmail.com>
2022-01-18 23:53:42 +0300

    Init project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app: express.Application = express()
+const address: string = "0.0.0.0:3000"
+
+app.use(bodyParser.json())
+
+app.get('/', function (req: express.Request, res: express.Response) {
+    res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+    console.log(`starting app on: ${address}`)
+})
